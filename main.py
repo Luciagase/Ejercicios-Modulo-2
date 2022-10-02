f869a61698d67977f445db36f96e6a8b703adb19
Luciagase <113980773+Luciagase@users.noreply.github.com>
2022-10-02 15:23:32 +0000

    main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #1) Utiliza el metodo input()
 #1.A
-resultado=input('Escribe un numero:') #Al utilizar el método input, el programa espera a que el usuario introduzca un valor en el teclado. Si no lo introduces el programa no termina de leer el código. Para guiar al usuario se puede escribir una instrución entre los paréntesis del input que informará de que se necesita introducir un un valor.
+resultado=input('Escribe un numero:""') #Al utilizar el método input, el programa espera a que el usuario introduzca un valor en el teclado. Si no lo introduces el programa no termina de leer el código. Para guiar al usuario se puede escribir una instrución entre los paréntesis del input que informará de que se necesita introducir un un valor.
 print(resultado)
 print(type(resultado))
 
 #1.B
-
+Num=int(input('Escribe un numero:'))
